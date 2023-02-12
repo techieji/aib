4d19ac649619d4aafea63aed8a56517bf0a96369
Pradhyumnan Rajasekar <pradhyumr@homeair.fios-router.home>
2023-02-12 14:19:39 -0500

    Page commit

--- basic-serve.py
+++ basic-serve.py
@@ -4,6 +4,12 @@ from linecache import getline
 
 s = '<script type="text/javascript" async="" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML-full"></script>'
 
+# hook('after_request')
+# def enable_cors():
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+#     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
 @get('/question/<n:int>')
 def frontend(n):
     d = json.loads(getline('data.jsonl', n))
@@ -13,6 +19,9 @@ def frontend(n):
 
 @get('/api/question/<n:int>')
 def question_api(n):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     return json.loads(getline('data.jsonl', n))
 
 run()
--- front-end/src/routes/questionbank/[id]/+page.js
+++ front-end/src/routes/questionbank/[id]/+page.js
@@ -1,4 +1,4 @@
-export function load({ fetch, params }) {
-    let resp = fetch(`http://127.0.0.1:8080/api/question/${params.id}`);
+export async function load({ fetch, params }) {
+    let resp = await fetch(`http://127.0.0.1:8080/api/question/${params.id}`);
     return resp.json();
 }
